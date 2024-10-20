861d3827fae41b5bd62fc1a2eb549154fc0c1f06
Nasen7072 <148974860+Nasen7072@users.noreply.github.com>
2024-10-20 02:14:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,23 +20,4 @@ function changeSlide(direction) {
 // Alternância de Tema (Claro <-> Escuro)
 const toggleThemeButton = document.getElementById('toggle-theme');
 
-// Verifica o tema armazenado no localStorage ao carregar a página
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-    toggleThemeButton.textContent = 'Modo Claro';
-} else {
-    toggleThemeButton.textContent = 'Modo Escuro';
-}
-
-// Função para alternar o tema
-toggleThemeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    
-    if (document.body.classList.contains('dark-mode')) {
-        toggleThemeButton.textContent = 'Modo Claro';
-        localStorage.setItem('theme', 'dark');
-    } else {
-        toggleThemeButton.textContent = 'Modo Escuro';
-        localStorage.setItem('theme', 'light');
-    }
-});
+// Verifica o tema
