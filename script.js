bd6adfd96d95baca290f549d27b005fbcf3a1eed
Nasen7072 <148974860+Nasen7072@users.noreply.github.com>
2024-10-20 22:44:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.nav-list');
 
+// Alterna a exibição do menu em dispositivos móveis
 if (mobileMenu) {
     mobileMenu.addEventListener('click', () => {
         navList.classList.toggle('active');
     });
 
-    // Para lidar com o clique nos itens de menu e mostrar o submenu
+    // Adiciona lógica para mostrar o submenu ao clicar
     document.querySelectorAll('.nav-list li').forEach(item => {
-        item.addEventListener('click', () => {
+        item.addEventListener('click', (e) => {
             const dropdown = item.querySelector('.dropdown');
             if (dropdown) {
-                dropdown.classList.toggle('active'); // Alternar o submenu
+                e.stopPropagation(); // Impede a propagação do clique
+                item.classList.toggle('active'); // Alterna a classe para mostrar/ocultar o submenu
             }
         });
     });
